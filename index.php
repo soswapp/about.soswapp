61c40615a161d411843a7d32e75a036394779190
Ikechukwu Okalia <dev@ikechukwuokalia.com>
2020-09-10 12:09:59 +0100

    bug fix

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ $page_name = "aboutus";
     <link rel="stylesheet" href="<?php echo \html_style("base.css"); ?>">
   </head>
   <body>
-    <?php \TymFrontiers\Helper\setup_page('about-us','base', true, PRJ_HEADER_HEIGHT); ?>
+    <?php \TymFrontiers\Helper\setup_page('aboutus','base', true, PRJ_HEADER_HEIGHT); ?>
     <?php include PRJ_INC_HEADER; ?>
     <section id="main-content">
       <div class="view-space">
